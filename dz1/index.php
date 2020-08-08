7eff699757c6f9ebe52a7dc943a31d3f189f339f
Kleossa <kleossa@yandex.ru>
2020-08-08 09:57:18 +0700

    dz1

---
+++ dz1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+       <?php
+    echo "Hello? World!";
+    ?> 
+    
+    <br>
+    
+    <?php
+    $name = "GeekBrains user";
+    echo "Hello, $name!";
+    ?>
+    
+    <br>
+    
+    <?php
+    define('MY_CONST', 100);
+    echo MY_CONST;
+    ?>
+
+    
+</body>
+</html>
+
+
+
